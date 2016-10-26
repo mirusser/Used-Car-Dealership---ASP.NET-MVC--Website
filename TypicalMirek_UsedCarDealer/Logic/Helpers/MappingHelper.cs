6a17cd92abf1403ca3f313c95c03c269ab0ee5ad
Mirek <mirusser@gmail.com>
2016-10-26 21:57:20 +0200

    #Development-Mirek Create car view model (partialy made)

--- TypicalMirek_UsedCarDealer/Logic/Helpers/MappingHelper.cs
+++ TypicalMirek_UsedCarDealer/Logic/Helpers/MappingHelper.cs
@@ -11,9 +11,18 @@ namespace TypicalMirek_UsedCarDealer.Logic.Helpers
     {
         public static Car MappAddingCarViewModelToCarModel(AddCarViewModel addCarViewModel)
         {
+            //TODO fill another properties of Car class
             return new Car()
             {
-
+                MainData = new MainData()
+                {
+                    TypeId = addCarViewModel.TypeId,
+                    CharacterId = addCarViewModel.CharacterId,
+                    ModelId = addCarViewModel.ModelId
+                },
+                BodyId = addCarViewModel.BodyId,
+                PropulsionId = addCarViewModel.PropulsionId,
+                SourceOfEnergyId = addCarViewModel.SourceOfEnergyId
             };
         }
     }
--- TypicalMirek_UsedCarDealer/Models/ViewModels/AddCarViewModel.cs
+++ TypicalMirek_UsedCarDealer/Models/ViewModels/AddCarViewModel.cs
@@ -11,30 +11,78 @@ namespace TypicalMirek_UsedCarDealer.Models.ViewModels
     {
         public int Id { get; set; }
 
+        #region Additional Data
+        [Display(Name = "Number of Seats")]
+        public int? NumberOfSeats { get; set; }
+        [Display(Name = "Number of previous owners")]
+        public int? NumberOfOwners { get; set; }
+        [Display(Name = "Engine Capacity")]
+        public decimal? EngineCapacity { get; set; }
+        [Display(Name = "Fuel tank capacity")]
+        public decimal? FuelTankCapacity { get; set; }
+        [Display(Name = "Engine power")]
+        public int? EnginePower { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:0.00}")]
+        public decimal? Length { get; set; }
+        public decimal? Mass { get; set; }
+        public decimal? Milleage { get; set; }
+        public bool? Damaged { get; set; }
+
+        [Display(Name = "Color")]
+        public int? ColorId { get; set; }
+        public IQueryable<SelectListItem> Colors { get; set; }
+        [Display(Name = "Gearbox")]
+        public int? GearboxId { get; set; }
+        public IQueryable<SelectListItem> Gearboxes { get; set; }
+        [Display(Name = "Country")]
+        public int? CountryId { get; set; }
+        public IQueryable<SelectListItem> Countries { get; set; }
+        [Display(Name = "Position of steering wheel")]
+        public int? PositionOfSteeringWheelId { get; set; }
+        public IQueryable<SelectListItem> PositionsOfSteeringWheel { get; set; }
+
+        #region Additional equipment
+        public bool? Climatisation { get; set; }
+        public bool? AdditionalWheels { get; set; }
+        public bool? Towbar { get; set; }
+        public bool? Radio { get; set; }
+        #endregion
+
+        #endregion
+
         #region MainData
         [Display(Name = "Type")]
         public int TypeId { get; set; }
-        public IEnumerable<SelectListItem> Types { get; set; }
+        public IQueryable<SelectListItem> Types { get; set; }
         [Display(Name = "Character")]
         public int CharacterId { get; set; }
-        public IEnumerable<SelectListItem> Characters { get; set; }
+        public IQueryable<SelectListItem> Characters { get; set; }
+
+        #region Model
+        //Model?
         [Display(Name = "Model")]
         public int ModelId { get; set; }
-        public IEnumerable<SelectListItem> Models { get; set; }
+        public IQueryable<SelectListItem> Models { get; set; }
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 3)]
+        public string ModelName { get; set; }
+        public string Version { get; set; }
+
         [Display(Name = "Brand")]
         public int BrandId { get; set; }
-        public IEnumerable<SelectListItem> Brands { get; set; }
+        public IQueryable<SelectListItem> Brands { get; set; }
+        #endregion
         #endregion
 
         [Display(Name = "Body")]
         public int BodyId { get; set; }
-        public IEnumerable<SelectListItem> Bodies { get; set; }
+        public IQueryable<SelectListItem> Bodies { get; set; }
         [Display(Name = "Propulsion")]
         public int PropulsionId { get; set; }
-        public IEnumerable<SelectListItem> Propulsions { get; set; }
+        public IQueryable<SelectListItem> Propulsions { get; set; }
         [Display(Name = "Source of energy")]
         public int SourceOfEnergyId { get; set; }
-        public IEnumerable<SelectListItem> SourcesOfEnergy { get; set; }
+        public IQueryable<SelectListItem> SourcesOfEnergy { get; set; }
 
         public ICollection<CarPhoto> Photos { get; set; }
     }
