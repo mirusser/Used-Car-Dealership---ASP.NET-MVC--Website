5832ff665aa0df3da11a19aeeb13039878df8c8d
pawel.maciejewski <pawel.maciejewski@aveneo.pl>
2017-01-05 05:23:25 +0100

    #development-Pawel almost ready site search car (basic search + webpagination) - need small changes and add rest of options

--- TypicalMirek_UsedCarDealer/Logic/Controllers/CarController.cs
+++ TypicalMirek_UsedCarDealer/Logic/Controllers/CarController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using TypicalMirek_UsedCarDealer.Logic.Factories.Interfaces;
 using TypicalMirek_UsedCarDealer.Logic.Managers;
 using TypicalMirek_UsedCarDealer.Logic.Managers.Interfaces;
+using TypicalMirek_UsedCarDealer.Logic.Repositories;
+using TypicalMirek_UsedCarDealer.Logic.Repositories.Interfaces;
+using TypicalMirek_UsedCarDealer.Models;
 using TypicalMirek_UsedCarDealer.Models.ViewModels;
 
 namespace TypicalMirek_UsedCarDealer.Logic.Controllers
@@ -13,10 +16,40 @@ namespace TypicalMirek_UsedCarDealer.Logic.Controllers
     public class CarController : Controller
     {
         private readonly ICarManager carManager;
+        private readonly IBrandManager brandManager;
+        private readonly ISourceOfEnergyRepository sourceOfEnergyRepository;
+        private readonly ICarBodyManager carBodyManager;
 
         public CarController(IManagerFactory managerFactory)
         {
             carManager = managerFactory.Get<CarManager>();
+            brandManager = managerFactory.Get<BrandManager>();
+            sourceOfEnergyRepository = new SourceOfEnergyRepository();
+            carBodyManager = managerFactory.Get<CarBodyManager>();
+        }
+
+        /// <summary>
+        /// Used by plugin dependent-dropdown to create Models dropdownlist
+        /// when user choosed Brand from first dropdownlist
+        /// more info: http://plugins.krajee.com/dependent-dropdown
+        /// </summary>
+        /// <param name="depdrop_parents">table of string but onlyone string is used</param>
+        /// <param name="depdrop_all_params">associative array of keys and values (it merges the values of depdrop_parents and depdrop_params) - to create an array</param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Models(string[] depdrop_parents, Dictionary<string, string> depdrop_all_params)
+        {
+            string brand = depdrop_parents[0];
+
+            var itemsToDropdown = new ItemsToDependendDropdownViewModel {output = new List<Output>()};
+            foreach (var it in carManager.GetAllCars().Where(it => it.MainData.Model.Brand.Name == brand).Select(it => it.MainData.Model.Name).Distinct())
+            {
+                itemsToDropdown.output.Add(new Output {id = it, name = it});
+            }
+            var firstOrDefault = itemsToDropdown.output.FirstOrDefault();
+            if (firstOrDefault != null) itemsToDropdown.selected = firstOrDefault.id;
+
+            return Json(itemsToDropdown, JsonRequestBehavior.AllowGet);
         }
 
         // GET: ShowCar
@@ -38,8 +71,11 @@ namespace TypicalMirek_UsedCarDealer.Logic.Controllers
             return View(car);
         }
 
-        public ActionResult CarList(string orderBy = null, int? minPrice = null, int? maxPrice = null, string brand = null, string model = null, int? page = null, int? perPage = null)
+        public ActionResult CarList(string orderBy = null, int? minPrice = null, int? maxPrice = null, int? minYear = null, int? maxYear = null, string brand = null, string model = null, string fuel = null, string body = null, int? page = null, int? perPage = null)
         {
+            var parametersToCarList = new ParametersToCarList();
+
+            #region Sort Car List
             var cars = new List<CarDetailsViewModel>();
 
             foreach (var it in carManager.GetAllCars().Where(it => it.DeleteTime == null))
@@ -69,6 +105,8 @@ namespace TypicalMirek_UsedCarDealer.Logic.Controllers
                     break;
             }
 
+            parametersToCarList.SortByList = new List<string>(new [] { "newFirst", "oldFirst", "priceAscending", "priceDescending", "alphabetical" });
+
             if (minPrice != null)
             {
                 cars = cars.Where(it => it.Price >= minPrice).ToList();
@@ -89,9 +127,65 @@ namespace TypicalMirek_UsedCarDealer.Logic.Controllers
                 cars = cars.Where(it => it.ModelName == model).ToList();
             }
 
+            if (!string.IsNullOrEmpty(fuel))
+            {
+                cars = cars.Where(it => it.SourceOfEnergy == fuel).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(body))
+            {
+                cars = cars.Where(it => it.Body == body).ToList();
+            }
+
+            if (minYear != null)
+            {
+                cars = cars.Where(it => it.YearOfProduction >= minYear).ToList();
+            }
+
+            if (maxYear != null)
+            {
+                cars = cars.Where(it => it.YearOfProduction <= maxYear).ToList();
+            }
+            #endregion
+
+            #region Add brand list
+
+            parametersToCarList.BrandList = brandManager.GetAll().Select(it => it.Name);
+
+            #endregion
+
+            #region Add model list
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                parametersToCarList.ModelList = carManager.GetAllCars().Where(it => it.MainData.Model.Brand.Name == brand).Select(it => it.MainData.Model.Name).Distinct();
+            }
+
+            #endregion
+
+            #region Number of pages and current page and paginate car list
+
+            if (page == null)
+            {
+                parametersToCarList.CurrentPage = 0;
+            }
+            else
+            {
+                parametersToCarList.CurrentPage = Convert.ToInt32(page);
+            }
+
+            if (perPage == null)
+            {
+                parametersToCarList.NumberOfPages = Convert.ToInt32(Math.Ceiling(cars.Count / 20.0));
+            }
+            else
+            {
+                parametersToCarList.NumberOfPages = Convert.ToInt32(Math.Ceiling(cars.Count / Convert.ToDouble(perPage)));
+            }
+
             if (page != null && perPage != null)
             {
-                if (cars.Count() >= page*perPage)
+                if (cars.Count() >= page * perPage)
                 {
                     cars = cars.Skip(Convert.ToInt32(page * perPage)).ToList();
                 }
@@ -115,8 +209,23 @@ namespace TypicalMirek_UsedCarDealer.Logic.Controllers
             {
                 cars = cars.Take(Convert.ToInt32(perPage)).ToList();
             }
+            parametersToCarList.Cars = cars;
 
-            return View(cars);
+            #endregion
+
+            #region Add Fuel Types
+
+            parametersToCarList.FuelList = sourceOfEnergyRepository.GetAll().Select(it => it.Name);
+
+            #endregion
+
+            #region Add Body Types
+
+            parametersToCarList.BodyList = carBodyManager.GetAll().Select(it => it.Name);
+
+            #endregion
+
+            return View(parametersToCarList);
         }
     }
 }
---
+++ TypicalMirek_UsedCarDealer/Models/ParametersToCarList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TypicalMirek_UsedCarDealer.Models.ViewModels;
+
+namespace TypicalMirek_UsedCarDealer.Models
+{
+    public class ParametersToCarList
+    {
+        public List<CarDetailsViewModel> Cars { get; set; }
+        public int NumberOfPages { get; set; }
+        public int CurrentPage { get; set; }
+        public IQueryable<string> BrandList { get; set; }
+        public IQueryable<string> ModelList { get; set; }
+        public IQueryable<string> BodyList { get; set; }
+        public IQueryable<string> FuelList { get; set; }
+        public List<string> SortByList { get; set; }
+    }
+}
