e87e869599ae3fb90a77758fe5a1d0e871230720
pawel.maciejewski <pawel.maciejewski@aveneo.pl>
2017-01-25 03:01:40 +0100

    #Development-Pawel added full working google maps plugin to 'About' and create entities on database to handle it

---
+++ TypicalMirek_UsedCarDealer/Logic/Managers/Interfaces/IMarkersConfigurationManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypicalMirek_UsedCarDealer.Logic.Factories.Interfaces;
+using TypicalMirek_UsedCarDealer.Models;
+
+namespace TypicalMirek_UsedCarDealer.Logic.Managers.Interfaces
+{
+    public interface IMarkersConfigurationManager : IManager
+    {
+        IQueryable<MarkersConfiguration> GetAll();
+
+        MarkersConfiguration GetMapLocalization();
+
+        IQueryable<MarkersConfiguration> GetAllMarkers();
+
+        MarkersConfiguration AddMarker(MarkersConfiguration markersConfiguration);
+
+        MarkersConfiguration ModifyMarker(MarkersConfiguration markersConfiguration);
+
+        MarkersConfiguration AddOrModifyMapLocalization(MarkersConfiguration markersConfiguration);
+
+        void DeleteMarker(MarkersConfiguration markersConfiguration);
+
+        void Dispose();
+    }
+}
---
+++ TypicalMirek_UsedCarDealer/Models/ParametersToAbout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypicalMirek_UsedCarDealer.Models
+{
+    public class ParametersToAbout
+    {
+        public MarkersConfiguration MapLocalization { get; set; }
+        public IQueryable<MarkersConfiguration> Markerslocalizations { get; set; }
+        public string PageContent { get; set; }
+    }
+}
