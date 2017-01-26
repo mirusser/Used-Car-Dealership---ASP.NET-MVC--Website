9e8d796d5628b7abf3c8edd6638c86069e176237
Mirek <mirusser@gmail.com>
2017-01-26 01:07:27 +0100

    #Development-Mirek Style develop, added displaying equipment in "SHow Car/ShowCar.cshtml"

--- TypicalMirek_UsedCarDealer/Models/ViewModels/EmailFormViewModel.cs
+++ TypicalMirek_UsedCarDealer/Models/ViewModels/EmailFormViewModel.cs
@@ -4,9 +4,9 @@ namespace TypicalMirek_UsedCarDealer.Models.ViewModels
 {
     public class EmailFormViewModel
     {
-        [Required, Display(Name = "Your name")]
+        [Required, Display(Name = @"Your name")]
         public string FromName { get; set; }
-        [Required, Display(Name = "Your email"), EmailAddress]
+        [Required, Display(Name = @"Your email"), EmailAddress]
         public string FromEmail { get; set; }
         [Required]
         public string Message { get; set; }
